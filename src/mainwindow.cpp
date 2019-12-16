3d26edd4b2bcb321031531c9890090772fa59a1c
Kervala <983521+kervala@users.noreply.github.com>
2019-12-16 20:51:16 +0100

    Fixed: qOverload not found... under Qt 5.10

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -93,7 +93,7 @@ MainWindow::MainWindow() : QMainWindow(nullptr, Qt::WindowStaysOnTopHint), m_sto
 	connect(m_ui->startKeySequenceEdit, &QKeySequenceEdit::keySequenceChanged, this, &MainWindow::onStartKeyChanged);
 	connect(m_ui->positionKeySequenceEdit, &QKeySequenceEdit::keySequenceChanged, this, &MainWindow::onPositionKeyChanged);
 
-#if QT_VERSION >= QT_VERSION_CHECK(5, 7, 0)
+#if FALSE && QT_VERSION >= QT_VERSION_CHECK(5, 7, 0)
 	connect(m_ui->defaultDelaySpinBox, qOverload<int>(&QSpinBox::valueChanged), this, &MainWindow::onDelayChanged);
 #else
 	connect(m_ui->defaultDelaySpinBox, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged), this, &MainWindow::onDelayChanged);
